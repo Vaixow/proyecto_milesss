e8b7b1acff0deafcd41828fbfb74d07cf1545e73
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-12-16 19:42:51 -0300

    Revert "A VER FINAL"
    This reverts commit 19dada52bd5393aee69991e4bde7cf8ab943c8a9.

--- miles_project/asgi.py
+++ miles_project/asgi.py
@@ -1,17 +1,18 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 
-import calificaciones.routing
-from calificaciones.middleware import JwtAuthMiddleware
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miles_project.settings")
 
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import calificaciones.routing
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": JwtAuthMiddleware(
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             calificaciones.routing.websocket_urlpatterns
         )
