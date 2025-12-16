15ae5492d4500c23c16e8f3a3089a417ec29ec2e
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-12-16 19:42:46 -0300

    Revert "YA SI CON ESTO NO FUNCIONA VUELVE AL 2"
    This reverts commit e030e9e2a76e5bcf4043ee311af2eeecf6bb2fa3.

--- calificaciones/middleware.py
+++ calificaciones/middleware.py
@@ -1,25 +1,27 @@
+# calificaciones/middleware.py
 from urllib.parse import parse_qs
-from channels.middleware import BaseMiddleware
-from channels.db import database_sync_to_async
 from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class JwtAuthMiddleware(BaseMiddleware):
+class JwtAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
+
     async def __call__(self, scope, receive, send):
-        query_string = parse_qs(scope["query_string"].decode())
-        token = query_string.get("token")
+        query = parse_qs(scope["query_string"].decode())
+        token = query.get("token")
 
         if token:
             try:
                 access = AccessToken(token[0])
-                user = await database_sync_to_async(User.objects.get)(id=access["user_id"])
+                user = User.objects.get(id=access["user_id"])
                 scope["user"] = user
             except Exception:
                 scope["user"] = AnonymousUser()
         else:
             scope["user"] = AnonymousUser()
 
-        return await super().__call__(scope, receive, send)
+        return await self.inner(scope, receive, send)
--- calificaciones/routing.py
+++ calificaciones/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    path("ws/chat/", ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
 ]
