e030e9e2a76e5bcf4043ee311af2eeecf6bb2fa3
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-12-16 19:35:58 -0300

    YA SI CON ESTO NO FUNCIONA VUELVE AL 2

--- calificaciones/middleware.py
+++ calificaciones/middleware.py
@@ -1,27 +1,25 @@
-# calificaciones/middleware.py
 from urllib.parse import parse_qs
+from channels.middleware import BaseMiddleware
+from channels.db import database_sync_to_async
 from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class JwtAuthMiddleware:
-    def __init__(self, inner):
-        self.inner = inner
-
+class JwtAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        query = parse_qs(scope["query_string"].decode())
-        token = query.get("token")
+        query_string = parse_qs(scope["query_string"].decode())
+        token = query_string.get("token")
 
         if token:
             try:
                 access = AccessToken(token[0])
-                user = User.objects.get(id=access["user_id"])
+                user = await database_sync_to_async(User.objects.get)(id=access["user_id"])
                 scope["user"] = user
             except Exception:
                 scope["user"] = AnonymousUser()
         else:
             scope["user"] = AnonymousUser()
 
-        return await self.inner(scope, receive, send)
+        return await super().__call__(scope, receive, send)
--- miles_project/asgi.py
+++ miles_project/asgi.py
@@ -1,19 +1,19 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-import calificaciones.routing
-from calificaciones.middleware import JwtAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miles_project.settings")
 
+# 👉 Primero se inicializa Django
 django_asgi_app = get_asgi_application()
+
+# 👉 Recién después se importa Channels
+from channels.routing import ProtocolTypeRouter, URLRouter
+from calificaciones.routing import websocket_urlpatterns
+from calificaciones.middleware import JwtAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": JwtAuthMiddleware(
-        URLRouter(
-            calificaciones.routing.websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
