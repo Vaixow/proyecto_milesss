19dada52bd5393aee69991e4bde7cf8ab943c8a9
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-12-16 19:27:43 -0300

    A VER FINAL

--- miles_project/asgi.py
+++ miles_project/asgi.py
@@ -1,18 +1,17 @@
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import calificaciones.routing
+from calificaciones.middleware import JwtAuthMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miles_project.settings")
 
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import calificaciones.routing
-
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
+    "websocket": JwtAuthMiddleware(
         URLRouter(
             calificaciones.routing.websocket_urlpatterns
         )
