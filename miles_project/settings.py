4586cc9f3ca789ffba08d72f57ce96992b049152
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-12-16 22:55:37 -0300

    Update settings.py

--- miles_project/settings.py
+++ miles_project/settings.py
@@ -229,11 +229,18 @@ CSRF_TRUSTED_ORIGINS = [
 
 REDIS_URL = os.getenv("REDIS_URL")
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_URL],
+if REDIS_URL:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [REDIS_URL],
+            },
         },
-    },
-}
+    }
+else:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+        },
+    }
