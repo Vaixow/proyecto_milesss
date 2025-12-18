afa4215c32662637096ae1068fc32cc3d0c2ae55
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-12-17 23:50:14 -0300

    Revert "."
    This reverts commit 069698156cc0a815ffd0942b4658b56f1d6dbbd1.

--- miles_project/asgi.py
+++ miles_project/asgi.py
@@ -1,3 +1,4 @@
+# miles_project/asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -6,8 +7,10 @@ import calificaciones.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miles_project.settings")
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             calificaciones.routing.websocket_urlpatterns
