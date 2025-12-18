069698156cc0a815ffd0942b4658b56f1d6dbbd1
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-12-17 22:46:23 -0300

    .

--- miles_project/asgi.py
+++ miles_project/asgi.py
@@ -1,4 +1,3 @@
-# miles_project/asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -7,10 +6,8 @@ import calificaciones.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miles_project.settings")
 
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             calificaciones.routing.websocket_urlpatterns
